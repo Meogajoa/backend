271f17a45afe6be5e0ec5b0898460787bec12f7f
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2024-11-25 16:54:25 +0900

    Project Init

---
+++ naegamaja/src/main/java/naegamaja_server/naegamaja/NaegamajaApplication.java
@@ -0,0 +1,13 @@
+package naegamaja_server.naegamaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaegamajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaegamajaApplication.class, args);
+	}
+
+}
---
+++ naegamaja/src/test/java/naegamaja_server/naegamaja/NaegamajaApplicationTests.java
@@ -0,0 +1,13 @@
+package naegamaja_server.naegamaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaegamajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
