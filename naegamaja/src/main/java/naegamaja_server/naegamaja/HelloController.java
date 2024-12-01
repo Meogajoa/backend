69e13a8be3967f64da09e9364b7722d64d09a9ee
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2024-12-01 14:20:24 +0900

    임베디드 테스트 코드 수정 2

--- naegamaja/src/main/java/naegamaja_server/naegamaja/HelloController.java
+++ naegamaja/src/main/java/naegamaja_server/naegamaja/HelloController.java
@@ -22,34 +22,24 @@ public class HelloController {
         return "goodnight";
     }
 
-    import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-    @RestController
-    public class TestController {
-
-        private final HelloService helloService;
-
-        public TestController(HelloService helloService) {
-            this.helloService = helloService;
-        }
-
-        @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
-        @ResponseBody
-        public String test(@RequestParam(value = "value", required = false, defaultValue = "-1") int value){
-            if(value != -1){
-                helloService.sendMessageToAll("Received value: " + value);
-                return "Received value: " + value;
-            } else {
-                helloService.sendMessageToAll("No value received");
-                return "No value received";
-            }
-        }
+    @GetMapping(value = "/test0", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String test(){
+        helloService.sendMessageToAll("날라옴0");
+        return "1";
     }
 
+    @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String test(@RequestParam(value = "value", required = false, defaultValue = "-1") int value){
+        if(value != -1){
+            helloService.sendMessageToAll("Received value: " + value);
+            return "Received value: " + value;
+        } else {
+            helloService.sendMessageToAll("No value received");
+            return "No value received";
+        }
+    }
 
     @GetMapping("/tempo")
     public String tempo(){
