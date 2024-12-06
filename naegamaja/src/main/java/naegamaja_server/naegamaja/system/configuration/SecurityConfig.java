08cbefd0d8f250f457fd0d843c3716bf731f1125
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2024-12-07 03:58:58 +0900

    세션 관리 기능 중간커밋

--- naegamaja/src/main/java/naegamaja_server/naegamaja/system/configuration/SecurityConfig.java
+++ naegamaja/src/main/java/naegamaja_server/naegamaja/system/configuration/SecurityConfig.java
@@ -1,6 +1,8 @@
 package naegamaja_server.naegamaja.system.configuration;
 
 
+import naegamaja_server.naegamaja.system.security.filter.SessionIdAuthenticationFilter;
+import naegamaja_server.naegamaja.system.security.provider.SessionIdAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,16 +33,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(new SessionIdAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-
-        return http.build();
+                ).build();
     }
 }
