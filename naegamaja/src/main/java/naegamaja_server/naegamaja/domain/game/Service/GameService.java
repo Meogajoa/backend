9e4427d90831cbfe28117b474e3e4f0182227049
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2024-12-28 17:47:25 +0900

    [FEAT] redis, mysql testing and start making game logic

---
+++ naegamaja/src/main/java/naegamaja_server/naegamaja/domain/game/Service/GameService.java
@@ -0,0 +1,13 @@
+package naegamaja_server.naegamaja.domain.game.Service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+    @Async
+    public void startGame() {
+
+    }
+}
