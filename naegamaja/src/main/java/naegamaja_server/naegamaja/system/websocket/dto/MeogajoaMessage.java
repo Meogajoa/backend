039291a7284bb3632336b00ea151ca9b4f98b850
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2025-02-10 22:45:40 +0900

    [FEAT] redTeam userInfo 추가

--- naegamaja/src/main/java/naegamaja_server/naegamaja/system/websocket/dto/MeogajoaMessage.java
+++ naegamaja/src/main/java/naegamaja_server/naegamaja/system/websocket/dto/MeogajoaMessage.java
@@ -185,6 +185,7 @@ public class MeogajoaMessage {
         private String id;
         private List<Long> blackTeam;
         private List<Long> whiteTeam;
+        private List<Long> redTeam;
         private List<Long> eliminated;
     }
 
